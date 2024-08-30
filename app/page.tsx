c39df5d2a7fcd2b2ffbed637605907f422601526
Oscdivin <divineuwandu84@Gmail.com>
2024-08-30 01:29:08 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl text-red-300">Home pages</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
