0abe59e36d07946f0821d701ff8515310e02cbf0
Oscdivin <divineuwandu84@Gmail.com>
2024-09-02 15:06:18 +0100

    Navbar & Footer is been created now

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,13 @@
+import Footer from '@/components/ui/Footer'
+import Navbar from '@/components/ui/Navbar'
 import React from 'react'
 
 const layout = ({children } : {children : React.ReactNode}) => {
   return (
     <>
-    Navbar
+    <Navbar/>
     {children}
-    Footer
+    <Footer/>
     </>
 
 
