a0a853e4844863b67806494952d622d1ef0b5206
Oscdivin <divineuwandu84@Gmail.com>
2024-09-02 15:18:23 +0100

    Navbar file is been create now with styles

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,10 +1,21 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
+    <header className='fixed text-white flex-center w-full border-b-2 border-black-200 py-7 top-0 z-50'>
+        <nav className='flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16'>
+            <Link href="">
+            <Image src="/json-logo.svg"
+            width={55}
+            height={40}
+            alt='logo'/>
+            </Link>
+
+        </nav>
       
-    </div>
+    </header>
   )
 }
 
