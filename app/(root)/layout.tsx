eeb0728713f1194515a6bb45159498b8f8d73fe1
Oscdivin <divineuwandu84@Gmail.com>
2024-09-02 14:52:16 +0100

    some been create

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// const layout = ({children } : {children : React.ReactNode}) => {
+//   return (
+//     <>
+//     Navbar
+//     {children}
+//     Footer
+//     </>
+
+
+//   )
+// }
+
+// export default layout
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,80 +1,76 @@
-import type { Config } from "tailwindcss"
-
-const config = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   darkMode: ["class"],
+//   content: [
+//     "./pages/**/*.{ts,tsx}",
+//     "./components/**/*.{ts,tsx}",
+//     "./app/**/*.{ts,tsx}",
+//     "./src/**/*.{ts,tsx}",
+//   ],
+//   theme: {
+//     screens: {
+//       xs: "400px",
+//       sm: "640px",
+//       md: "768px",
+//       lg: "1024px",
+//       xl: "1220px",
+//       "2xl": "1440px",
+//       "3xl": "1700px",
+//     },
+//     container: {
+//       center: true,
+//       padding: "2rem",
+//       screens: {
+//         "2xl": "1400px",
+//       },
+//     },
+//     extend: {
+//       fontFamily: {
+//         poppins: ["Poppins", "sans-serif"],
+//         inter: ["Inter", "sans-serif"],
+//       },
+//       colors: {
+//         primary: "#2190FF",
+//         black: {
+//           DEFAULT: "#000",
+//           100: "#0D1117",
+//           200: "#161B22",
+//           300: "#1F2428",
+//           400: "#242C38",
+//         },
+//         grey: {
+//           100: "#969BA5",
+//           200: "#55616D",
+//         },
+//         white: {
+//           DEFAULT: "#FFF",
+//           400: "#A3B3BC",
+//           500: "#A4B8D5",
+//           800: "#D0DFFF",
+//         },
+//         purple: "#8C7CFF",
+//         pink: "#ED5FBD",
+//         violet: "#F16565",
+//         orange: "#FF964B",
+//       },
+//       backgroundImage: {
+//         banner: "url('/jsm_resources_banner.svg')",
+//       },
+//       keyframes: {
+//         "accordion-down": {
+//           from: { height: 0 },
+//           to: { height: "var(--radix-accordion-content-height)" },
+//         },
+//         "accordion-up": {
+//           from: { height: "var(--radix-accordion-content-height)" },
+//           to: { height: 0 },
+//         },
+//       },
+//       animation: {
+//         "accordion-down": "accordion-down 0.2s ease-out",
+//         "accordion-up": "accordion-up 0.2s ease-out",
+//       },
+//     },
+//   },
+//   plugins: [require("tailwindcss-animate")],
+// };
