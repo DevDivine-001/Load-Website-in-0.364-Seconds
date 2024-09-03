18540e05651779971259b5a470446e32bf12074e
Oscdivin <divineuwandu84@Gmail.com>
2024-09-03 22:38:07 +0100

    resource.schemas.ts is ready now

--- sanity/Schemas/resource.schemas.ts
+++ sanity/Schemas/resource.schemas.ts
@@ -19,6 +19,40 @@ const schema = {
       options: { sources: 'title' }
 
     },
+    {
+      name: "downloadLink",
+      title: "Download Link",
+      type: "url",
+      validation: (Rule: any) => Rule.required()
+
+    },
+    {
+      name: "views",
+      title: "Views",
+      type: "number",
+      initialValue: 0,
+
+    },
+    {
+      name: "poster",
+      title: "Poster",
+      type: "image",
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        hotspot: true,
+      }
+
+    },
+    {
+      name: "category",
+      title: "Category",
+      type: "string",
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        list: ['Frontend', 'Backend', 'UI/UX', 'Next 13', 'FullStack', 'Other'],
+      }
+
+    },
   ]
 
 }
