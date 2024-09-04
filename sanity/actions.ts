e13c81d6a511d94a4931651083274ab9f34a82f8
Oscdivin <divineuwandu84@Gmail.com>
2024-09-04 17:09:59 +0100

    resource.schemas with update

--- sanity/actions.ts
+++ sanity/actions.ts
@@ -1,36 +1,36 @@
-// import { groq } from 'next-sanity';
-// import { readClient } from './lib/client';
-// import { buildQuery } from './utils';
+import { groq } from 'next-sanity';
+import { readClient } from './lib/client';
+import { buildQuery } from './utils';
 
-// interface GetResourcesParams {
-//     query: string;
-//     category: string;
-//     page: string;
-// }
+interface GetResourcesParams {
+    query: string;
+    category: string;
+    page: string;
+}
 
-// export const getResources = async (params: GetResourcesParams) => {
-//     const { query, category, page } = params;
+export const getResources = async (params: GetResourcesParams) => {
+    const { query, category, page } = params;
 
-//     try {
-//         const resources = await readClient.fetch(
-//             groq`${buildQuery({
-//                 type: 'resource',
-//                 query,
-//                 category,
-//                 page: parseInt(page),
-//             })}{
-//         title,
-//         _id,
-//         downloadLink,
-//         "image": poster.asset->url,
-//         views,
-//         slug,
-//         category
-//       }`
-//         );
+    try {
+        const resources = await readClient.fetch(
+            groq`${buildQuery({
+                type: 'resource',
+                query,
+                category,
+                page: parseInt(page),
+            })}{
+        title,
+        _id,
+        downloadLink,
+        "image": poster.asset->url,
+        views,
+        slug,
+        category
+      }`
+        );
 
-//         return resources;
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+        return resources;
+    } catch (error) {
+        console.log(error);
+    }
+}
