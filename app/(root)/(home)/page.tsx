030963bc5dc77a1a10e596ecd3ba7c17d0f7805c
Oscdivin <divineuwandu84@Gmail.com>
2024-09-10 01:32:50 +0100

    changes title and remove it and fix the error

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,3 @@
-
 import Filters from '@/components/ui/Filters'
 import Header from '@/components/ui/Header'
 import ResourceCard from '@/components/ui/ResourceCard'
@@ -6,137 +5,90 @@ import SearchForm from '@/components/ui/SearchForm'
 import { getResources, getResourcesPlaylist } from '@/sanity/Schemas/actions'
 import React from 'react'
 
-export const revalidate = 10000
+export const revalidate = 100
+
+interface Resource {
+  _id: string;
+  title: string;
+  image: string;
+  views: number;
+  downloadLink: string;
+}
+
+interface Playlist {
+  _id: string;
+  title: string;
+  resources: Resource[];
+}
+
 
 interface IProps {
-  searchParams: {[ key: string] : string | undefined }
+  searchParams: { [key: string]: string | undefined }
 }
 
-
-const page = async ({ 
-  searchParams
-
-}: IProps) => {
-  const resources = await getResources({
+const page = async ({ searchParams }: IProps) => {
+  const resources: Resource[] = await getResources({
     query: searchParams?.query || "",
-    category: searchParams?.category ||
-     "",
-    page:"1" 
-  })
-  // console.log(resources)
+    category: searchParams?.category || "",
+    page: "1"
+  });
 
-  const resourcesPlaylist = await getResourcesPlaylist()
-  // console.log(resourcesPlaylist)
+  const resourcesPlaylist: Playlist[] = await getResourcesPlaylist();
+
   return (
-    <main className='
-    flex-center 
-    paddings 
-    mx-auto
-    w-full
-    max-w-screen-2xl
-    flex-col
-   '>
-   <section className='
-   nav-padding 
-   w-full
-   '>
-    <div className='
-    flex-center
-    relative
-    min-h-[344px]
-    w-full
-    flex-col
-    rounded-xl
-    bg-banner
-    bg-cover
-    bg-center
-    '>
-        <h1 className='
-        sm:heading1 
-        heading2
-        text-center
-        text-white
-        text-balance
-        font-bold
-        text-3xl
-        '
-        
-        >
-            JavaScript & TypeScript  Mastery Resources
-        </h1>
-    </div>
-    <SearchForm/>
-    </section>   
-    <Filters/>
-    {(searchParams?.query || searchParams?.category) && (
-          <section className='flex-center  w-full flex-col sm:mt-20 '>
-     < Header
-     
-     query={searchParams?.query || ''}
-     category={searchParams?.category || ""}
-     />
-      <div className='flex w-full flex-wrap mt-7 gap-12 sm:justify-start'>
-{resources?.length >0 ? (
-          
-          resources.map((resources:any) => (
-            <ResourceCard
-            key={resources._id}
-            title={resources.title}
-            id={resources._id}
-            image={resources.image}
-            downloadNumber={resources.views}
-          downLoadLink={resources.downloadLink}
-           
-            />
-          ))
-        
-    
-):(
-  <p className='body-regular text-white-400'>
-    No resources found
-  </p>
-)}
-
-      </div>
-    </section>
-    )}
-    { resourcesPlaylist.map((item: any) =>(
-      <section key={item._id} className='
-      flex-center mt-6 w-full flex-col sm:mt-20'>
-        <h1
-        className='heading3 self-start
-        text-white-800'>{item.title}</h1>
-        <div
-        className='mt-12 
-        flex 
-        w-full
-        flex-wrap
-        justify-center
-        gap-16
-        sm:justify-start
-        '>
-          {
-            item.resources.map((resources:any)=>(
-                 <ResourceCard
-            key={resources._id}
-            title={resources.title}
-            id={resources._id}
-            image={resources.image}
-            downloadNumber={resources.views}
-            downLoadLink={resources.downloadLink}
-           
-            />
-
-            ))
-          }
-          
+    <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
+      <section className='nav-padding w-full'>
+        <div className='flex-center relative min-h-[344px] w-full flex-col rounded-xl bg-banner bg-cover bg-center'>
+          <h1 className='sm:heading1 heading2 text-center text-white text-balance font-bold text-3xl'>
+            JavaScript & TypeScript Mastery Resources
+          </h1>
         </div>
-
+        <SearchForm />
       </section>
-    ))}
-
+      <Filters />
+      {(searchParams?.query || searchParams?.category) && (
+        <section className='flex-center w-full flex-col sm:mt-20'>
+          <Header
+            query={searchParams?.query || ''}
+            category={searchParams?.category || ""}
+          />
+          <div className='flex w-full flex-wrap mt-7 gap-12 sm:justify-start'>
+            {resources.length > 0 ? (
+              resources.map((resource) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  id={resource._id}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                  downLoadLink={resource.downloadLink}
+                />
+              ))
+            ) : (
+              <p className='body-regular text-white-400'>No resources found</p>
+            )}
+          </div>
+        </section>
+      )}
+      {resourcesPlaylist.map((item) => (
+        <section key={item._id} className='flex-center mt-6 w-full flex-col sm:mt-20'>
+          <h1 className='heading3 self-start text-white-800'>{item.title}</h1>
+          <div className='mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start'>
+            {item.resources.map((resource) => (
+              <ResourceCard
+                key={resource._id}
+                title={resource.title}
+                id={resource._id}
+                image={resource.image}
+                downloadNumber={resource.views}
+                downLoadLink={resource.downloadLink}
+              />
+            ))}
+          </div>
+        </section>
+      ))}
     </main>
-  )
+  );
 }
 
-export default page
+export default page;
--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -4,33 +4,33 @@ interface Props {
 }
 
 const Header = ({ query, category }: Props) => {
-  if(query && category) {
+  if (query && category) {
     return (
       <h1 className="heading3 self-start text-white-800">
-        Search results for "{query}" in <span className="capitalize">{category}</span>
+        Search results for &quot;{query}&quot; in <span className="capitalize">{category}</span>
       </h1>
-    )
+    );
   }
 
-  if(query) {
+  if (query) {
     return (
       <h1 className="heading3 self-start text-white-800">
-        Search results for "{query}"
+        Search results for &quot;{query}&quot;
       </h1>
-    )
+    );
   }
 
-  if(category) {
+  if (category) {
     return (
       <h1 className="heading3 self-start text-white-800">
         <span className="capitalize">{category}</span>
       </h1>
-    )
+    );
   }
 
   return (
     <h1 className="heading3 self-start text-white-800">No Results</h1>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
